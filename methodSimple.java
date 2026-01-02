efd0931e8047f3399f86121ccd2dd1bdc5536005
Arpitsingh16 <arpitchauhan1607@gmail.com>
2026-01-02 18:14:59 +0530

    Updated

---
+++ methodSimple.java
@@ -0,0 +1,11 @@
+public class methodSimple {
+    void isRunning(){
+        System.out.println("Help i'm running");
+    }
+
+    public static void main(String[] args) {
+        methodSimple ms = new methodSimple();
+        ms.isRunning();
+    }
+    
+}
