729fb18463053a2e8e86f3e3d26f1bbb1450a358
Arpitsingh16 <arpitchauhan1607@gmail.com>
2026-01-03 19:12:39 +0530

    Creating constructor named Store

---
+++ Store.java
@@ -0,0 +1,12 @@
+    public class Store{
+  public Store(){
+    System.out.println("I am inside the Store constructor");
+  }
+  
+  public static void main(String[] args) {
+    Store s1 = new Store();
+    System.out.println(s1);
+  }
+}
+    
+
