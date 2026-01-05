662a1685c78105cdf2acc9e3a95f18207be4e6d2
Arpitsingh16 <arpitchauhan1607@gmail.com>
2026-01-05 17:34:49 +0530

    Created Animal and AnimalMain.java

---
+++ Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    private String breed;
+    private String name;
+    private double price;
+
+
+
+    public Animal(String breed, String name, double price){
+        this.breed = breed;
+        this.name = name;
+        this.price = price;
+    }
+
+    void bark(){
+        System.out.println(name+" is a dog his breed is " + breed + " his price is " + price);
+    }
+
+    
+}
---
+++ AnimalMain.java
@@ -0,0 +1,8 @@
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal dog = new Animal("BullDog", "Tommy", 200000);
+
+        dog.bark();
+    }
+    
+}
