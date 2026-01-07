c74cc4ae10358bd2f7c5f8f839b9d107c0dbd6a8
Arpitsingh16 <arpitchauhan1607@gmail.com>
2026-01-07 22:02:51 +0530

    Updated methodSimple Page

--- methodSimple.java
+++ methodSimple.java
@@ -1,11 +1,21 @@
 public class methodSimple {
+    private int id;
+    private String name;
+
+
     void isRunning(){
         System.out.println("Help i'm running");
     }
 
+    public String getName(int id, String name){
+        this.id = id;
+        System.out.print("Name is " + name + " id is " + id);
+        return "";
+    }
     public static void main(String[] args) {
         methodSimple ms = new methodSimple();
         ms.isRunning();
+        ms.getName(200, "Arpit");
     }
     
 }
